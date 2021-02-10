3914e7077e75517ee1b17be1654e9ac4031ed3f5
Ikey Doherty <ikey@serpentos.com>
2021-02-10 20:32:17 +0000

    binary/reader: Remove enumeration abilities, stop reading payploads
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/format/binary/reader.d
+++ source/moss/format/binary/reader.d
@@ -29,39 +29,6 @@ public import moss.format.binary.header;
 import moss.format.binary.endianness;
 
 /**
- * Encapsulates the underlying Payload so we can provide some tracking
- * and iteration abiliies
- */
-struct PayloadWrapper
-{
-    /** Extend Payload with PayloadWrapper functionality */
-    Payload pt;
-    alias pt this;
-
-package:
-
-    /**
-     * Returns the 'read' property, i.e. was we decoded or not
-     */
-    pure @property bool read() @safe @nogc nothrow
-    {
-        return _read;
-    }
-
-    /**
-     * Update the read property
-     */
-    pure @property void read(bool b) @safe @nogc nothrow
-    {
-        _read = b;
-    }
-
-private:
-
-    bool _read = false;
-}
-
-/**
  * The Reader is a low-level mechanism for parsing Moss binary packages.
  */
 final class Reader
@@ -71,36 +38,6 @@ private:
 
     File _file;
     Header _header;
-    int16_t payloadIndex = -1;
-    int16_t requestedIndex = 0;
-    PayloadWrapper curPayload;
-
-    /**
-     * Load the current payload
-     */
-    Payload loadPayload() @trusted
-    {
-        import std.stdio : fread;
-        import std.exception : enforce;
-
-        scope auto fp = _file.getFP();
-
-        Payload ret;
-        enforce(fread(&ret, Payload.sizeof, 1, fp) == 1, "nextPayload(): Failed to read");
-        ret.toHostOrder();
-
-        return ret;
-    }
-
-    /**
-     * Skip the contents of the payload completely
-     */
-    void skipPayload() @trusted
-    {
-        import std.stdio : fseek, SEEK_CUR;
-
-        _file.seek(curPayload.length, SEEK_CUR);
-    }
 
 public:
     @disable this();
@@ -124,47 +61,11 @@ public:
 
         _header.toHostOrder();
         _header.validate();
-
-        curPayload = PayloadWrapper();
     }
 
     ~this() @safe
     {
         close();
-    }
-
-    /**
-     * Return the current entry in the reader
-     */
-    @property PayloadWrapper front()
-    {
-        if (requestedIndex != payloadIndex)
-        {
-            if (requestedIndex > 0 && !curPayload.read)
-            {
-                skipPayload();
-            }
-            curPayload.pt = loadPayload();
-            payloadIndex = requestedIndex;
-        }
-        return curPayload;
-    }
-
-    /**
-     * Return true if there are no more entries
-     */
-    @property bool empty()
-    {
-        return requestedIndex >= _header.numPayloads;
-    }
-
-    /**
-     * Pop the current entry and find the next
-     */
-    @property PayloadWrapper popFront()
-    {
-        ++requestedIndex;
-        return curPayload;
     }
 
     /**
