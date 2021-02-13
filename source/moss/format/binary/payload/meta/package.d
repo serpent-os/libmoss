1e6e35ed8960d74d0e59059b0d47926172176cbb
Ikey Doherty <ikey@serpentos.com>
2021-02-13 23:28:52 +0000

    meta/package: Add the new addRecord method
    The new addRecord method is ported from our legacy codebase to allow
    a generic approach to compiler-enforced runtime validatio of types
    and tag context usage for a Record.
    
    In short, we glitter some magic on the API to allow automatic type
    validation to take place.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/format/binary/payload/meta/package.d
+++ source/moss/format/binary/payload/meta/package.d
@@ -85,7 +85,128 @@ public:
         writeln("MetaPayload.encode(): IMPLEMENT ME");
     }
 
+    /**
+     * Add a new Record to the pair set for future encoding
+     */
+    void addRecord(R : RecordTag, T)(R key, auto const ref T datum) @system
+    {
+        import std.traits : EnumMembers, OriginalType;
+        import std.stdio : writeln;
+        import std.conv : to;
+
+        pairs ~= RecordPair();
+        auto pair = &pairs[pairs.length - 1];
+        pair.tag = key;
+
+        static foreach (i, m; EnumMembers!RecordTag)
+        {
+            if (i == key)
+            {
+
+                mixin("enum memberName = __traits(identifier, EnumMembers!RecordTag[i]);");
+                mixin("enum attrs = __traits(getAttributes, RecordTag." ~ to!string(
+                        memberName) ~ ");");
+                static assert(attrs.length == 1,
+                        "Missing validation tag for RecordTag." ~ to!string(memberName));
+
+                static if (is(T == string))
+                {
+                    switch (attrs[0])
+                    {
+
+                    case RecordType.String:
+                        assert(typeid(OriginalType!T) == typeid(string),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects string, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_string = datum;
+                        pair.type = RecordType.String;
+                        break;
+                    default:
+                        assert(0, "INCOMPLETE SUPPORT");
+                    }
+                }
+                else
+                {
+                    switch (attrs[0])
+                    {
+
+                    case RecordType.Int8:
+                        assert(typeid(OriginalType!T) == typeid(int8_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects int8_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_i8 = cast(int8_t) datum;
+                        pair.type = RecordType.Int8;
+                        break;
+                    case RecordType.Uint8:
+                        assert(typeid(OriginalType!T) == typeid(uint8_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects uint8_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_u8 = cast(uint8_t) datum;
+                        pair.type = RecordType.Uint8;
+                        break;
+
+                    case RecordType.Int16:
+                        assert(typeid(OriginalType!T) == typeid(int16_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects int16_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_i16 = cast(int16_t) datum;
+                        pair.type = RecordType.Int16;
+                        break;
+                    case RecordType.Uint16:
+                        assert(typeid(OriginalType!T) == typeid(uint16_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects uint16_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_u16 = cast(uint16_t) datum;
+                        pair.type = RecordType.Uint16;
+                        break;
+                    case RecordType.Int32:
+                        assert(typeid(OriginalType!T) == typeid(int32_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects int32_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_i32 = cast(int32_t) datum;
+                        pair.type = RecordType.Int32;
+                        break;
+                    case RecordType.Uint32:
+                        assert(typeid(OriginalType!T) == typeid(uint32_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects uint32_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_u32 = cast(uint32_t) datum;
+                        pair.type = RecordType.Uint32;
+                        break;
+                    case RecordType.Int64:
+                        assert(typeid(OriginalType!T) == typeid(int64_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects int64_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_i64 = cast(int64_t) datum;
+                        pair.type = RecordType.Int64;
+                        break;
+                    case RecordType.Uint64:
+                        assert(typeid(OriginalType!T) == typeid(uint64_t),
+                                "addRecord(RecordTag." ~ memberName ~ ") expects uint64_t, not " ~ typeof(datum)
+                                .stringof);
+                        writeln("Writing key: ", key, " - value: ", datum);
+                        pair.val_u64 = cast(uint64_t) datum;
+                        pair.type = RecordType.Uint64;
+                        break;
+                    default:
+                        assert(0, "INCOMPLETE SUPPORT");
+                    }
+                }
+            }
+        }
+    }
+
 private:
 
     RecordPair[] pairs;
 }
+
+public import moss.format.binary.payload.meta.record;
