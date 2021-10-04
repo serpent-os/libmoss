e28fb10a56e5c1a63affc49d64d9e77bee10327a
Ikey Doherty <ikey@serpentos.com>
2021-10-04 10:59:00 +0100

    query: Add all public imports
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/deps/query/package.d
+++ source/moss/deps/query/package.d
@@ -24,4 +24,5 @@ module moss.deps.query;
 
 public import moss.deps.query.candidate;
 public import moss.deps.query.components;
+public import moss.deps.query.manager;
 public import moss.deps.query.source;
