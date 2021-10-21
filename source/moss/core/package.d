25b7defbfe120a81e837d1206a7c2e006a254c7a
Ikey Doherty <ikey@serpentos.com>
2021-10-21 11:58:28 +0100

    core: Introduce FileType enum
    This is core to all of our consumers and needs sharing as such
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/core/package.d
+++ source/moss/core/package.d
@@ -28,6 +28,8 @@ public import moss.core.util;
 public import moss.core.platform;
 public import moss.core.store;
 
+public import std.stdint : uint8_t;
+
 /** Current Moss Version */
 const Version = "0.0.1";
 
@@ -63,3 +65,33 @@ const RootStore = RootTree ~ "/root";
  * The DownloadStore directory contains all downloads
  */
 const DownloadStore = RootTree ~ "/download";
+
+/**
+ * Well known file type
+ */
+enum FileType : uint8_t
+{
+    /* Catch errors */
+    Unknown = 0,
+
+    /** Regular file **/
+    Regular = 1,
+
+    /** Symbolic link to another location */
+    Symlink = 2,
+
+    /** Directory */
+    Directory = 3,
+
+    /** Character Device */
+    CharacterDevice = 4,
+
+    /** Block device */
+    BlockDevice = 5,
+
+    /** Fifo pipe */
+    Fifo = 6,
+
+    /** Socket */
+    Socket = 7,
+}
