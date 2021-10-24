516807e84c80eb4d5eaf29cc4d767872c7fab689
Ikey Doherty <ikey@serpentos.com>
2021-10-24 16:28:10 +0100

    analysis: Fix imports
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/deps/analysis/package.d
+++ source/moss/deps/analysis/package.d
@@ -23,5 +23,6 @@
 module moss.deps.analysis;
 
 public import moss.deps.analysis.analyser;
+public import moss.deps.analysis.bucket;
 public import moss.deps.analysis.chain;
 public import moss.deps.analysis.fileinfo;
