a3323646f818ba0466ccf87446480b1907c9dca9
Ikey Doherty <ikey@serpentos.com>
2021-11-07 17:09:56 +0000

    registry: Fix that..
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/deps/registry/manager.d
+++ source/moss/deps/registry/manager.d
@@ -15,9 +15,9 @@
  *    claim that you wrote the original software. If you use this software
  *    in a product, an acknowledgment in the product documentation would be
  *    appreciated but is not required.
- * 2. Altered plugin versions must be plainly marked as such, and must not be
+ * 2. Altered source versions must be plainly marked as such, and must not be
  *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any plugin distribution.
+ * 3. This notice may not be removed or altered from any source distribution.
  */
 
 module moss.deps.registry.manager;
