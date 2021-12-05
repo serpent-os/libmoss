42d6f5ba13aa378b095004bc910da16f9558cbd4
Ikey Doherty <ikey@serpentos.com>
2021-12-05 13:27:29 +0000

    c: Fix up some useful imports
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/core/c.d
+++ source/moss/core/c.d
@@ -28,7 +28,7 @@ module moss.core.c;
 public import core.sys.posix.sys.types : dev_t, mode_t, off_t, slong_t;
 public import core.sys.posix.sys.stat : stat_t;
 public import core.sys.posix.fcntl : AT_SYMLINK_NOFOLLOW, AT_FDCWD, O_RDONLY,
-    O_RDWR, O_CREAT, O_CLOEXEC;
+    O_RDWR, O_WRONLY, O_CREAT, O_CLOEXEC, O_TRUNC;
 
 public import core.sys.posix.fcntl : open;
 public import core.sys.posix.unistd : close, unlink;
