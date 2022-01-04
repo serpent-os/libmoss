3a897e060b405b8478224690b771242ff45329bb
Ikey Doherty <ikey@serpentos.com>
2022-01-04 02:28:20 +0000

    repo: Split type from package
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/config/repo/type.d
+++ source/moss/config/repo/type.d
@@ -20,6 +20,41 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
-module moss.config.repo;
+module moss.config.repo.type;
 
-public import moss.config.repo.type;
+import std.string : format;
+import moss.config.io.schema;
+
+/**
+ * Holds all the relevant details for Repository deserialisation from
+ * a set of YML files
+ */
+@DomainKey("repos") public struct Repository
+{
+    /**
+     * Unique identifier for the repository
+     */
+    string id = null;
+
+    /**
+     * A human description for this repository
+     */
+    string description = null;
+
+    /**
+     * Where does one find said repository
+     */
+    @YamlSchema("uri", true) string uri = null;
+
+    /**
+     * Return a human readable description of the repo
+     */
+    pure @property auto toString()
+    {
+        if (description !is null)
+        {
+            return format!"%s - \"%s\" (%s)"(id, uri, description);
+        }
+        return format!"%s - \"%s\""(id, uri);
+    }
+}
