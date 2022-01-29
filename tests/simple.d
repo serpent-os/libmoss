271e205ad802d90a90034e461e91128418330ad5
Ikey Doherty <ikey@serpentos.com>
2022-01-29 23:00:32 +0000

    Convert unit tests into proper test files for meson
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ tests/simple.d
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Basic moss-config tests
+ *
+ * Simple use-case moss-config testing
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module simple;
+
+@("Primitve testing")
+unittest
+{
+    assert(0 == 0);
+}
---
+++ tests/test_main.d
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Main entry point
+ *
+ * Completely empty to support all compilers with meson
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module main;
+
+void main(){}
