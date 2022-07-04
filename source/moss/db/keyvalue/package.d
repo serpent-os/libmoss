cd424ec870bbb2f60230a88ba8b84969e5d5b379
Ikey Doherty <ikey@serpentos.com>
2022-07-04 12:14:36 +0100

    db: Start a new keyvalue submodule
    We're going to replace the utter jankery that is our current
    implementation with a new keyvalue module and RocksDB specific
    driver.
    
    This will allow us to nuke rocksdb-binding from orbit and use
    only the functionality we need.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/db/keyvalue/package.d
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.db.keyvalue package
+ *
+ * Module namespace imports.
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
