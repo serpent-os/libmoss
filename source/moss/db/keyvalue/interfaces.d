f34a72b42c2622c7f0bb986ada87a07d47b260bf
Ikey Doherty <ikey@serpentos.com>
2022-07-05 22:57:14 +0100

    keyvalue/interfaces: Add Bucket and BucketIterator APIs
    A Bucket is merely an identifier for a bucket, not an API in itself.
    The BucketIterator is essentially a forward range though we'll likely
    make it reversable too.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/db/keyvalue/interfaces.d
+++ source/moss/db/keyvalue/interfaces.d
@@ -15,5 +15,47 @@
 
 module moss.db.keyvalue.interfaces;
 
-import moss.db.keyvalue.errors;
+public import moss.core.encoding : ImmutableDatum, Datum;
+public import moss.db.keyvalue.errors;
+public import std.typecons : Tuple;
 
+/**
+ * A "Bucket" is merely a referential identifier for
+ * a compartment of storage in the database and offers
+ * no direct API itself.
+ *
+ */
+public struct Bucket
+{
+    /**
+     * Bucket identifier
+     */
+    ImmutableDatum prefix;
+}
+
+/**
+ * Simplistic interface. Iterators are owned by the
+ * implementation and should *not* be destroyed.
+ */
+public interface BucketIterator
+{
+    /**
+     * All iterations are performed with key and value in lockstep
+     */
+    static alias KeyValuePair = Tuple!(ImmutableDatum, "key", ImmutableDatum, "value");
+
+    /**
+     * Does this range have more to yield
+     */
+    pure bool empty() @safe nothrow return @nogc;
+
+    /**
+     * Front pair of the range
+     */
+    pure KeyValuePair front() @safe nothrow return @nogc;
+
+    /**
+     * Pop the front elemnt and move it along
+     */
+    pure void popFront() @safe nothrow return @nogc;
+}
