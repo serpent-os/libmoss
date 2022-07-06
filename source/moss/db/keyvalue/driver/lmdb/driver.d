77f40765c016fbcdcde262853e84133a94fea644
Ikey Doherty <ikey@serpentos.com>
2022-07-06 20:26:49 +0100

    lmdb: Split driver up more
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/db/keyvalue/driver/lmdb/driver.d
@@ -0,0 +1,112 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.db.keyvalue.driver.lmdb.driver
+ *
+ * KModule level imports
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.db.keyvalue.driver.lmdb.driver;
+
+public import moss.db.keyvalue.driver;
+import moss.db.keyvalue.errors;
+import moss.db.keyvalue.interfaces;
+
+import std.conv : octal;
+import std.string : toStringz;
+
+import lmdb;
+
+import moss.db.keyvalue.driver.lmdb : lmdbStr;
+
+/**
+ * Implementation using LMDB
+ */
+public final class LMDBDriver : Driver
+{
+    /**
+     * Open LMDB environment + open from a given path
+     *
+     * Params:
+     *      uri = Resource locator string
+     *      flags = Flags that affect opening
+     * Returns: A nullable error
+     */
+    override DatabaseResult connect(const(string) uri, DatabaseFlags flags) @safe nothrow
+    {
+        /* no env dir */
+        int cFlags = MDB_NOSUBDIR;
+
+        /* Read-only? */
+        if ((flags & DatabaseFlags.ReadOnly) == DatabaseFlags.ReadOnly)
+        {
+            cFlags |= MDB_RDONLY;
+        }
+
+        /* no sync ? */
+        if ((flags & DatabaseFlags.DisableSync) == DatabaseFlags.DisableSync)
+        {
+            cFlags |= MDB_NOSYNC;
+        }
+
+        /* Create environment first */
+        int rc = () @trusted { return mdb_env_create(&env); }();
+        if (rc != 0)
+        {
+            return DatabaseResult(DatabaseError(DatabaseErrorCode.ConnectionFailed, lmdbStr(rc)));
+        }
+
+        /* Open 0600 connection to the DB */
+        rc = () @trusted {
+            return mdb_env_open(env, uri.toStringz, cFlags, octal!600);
+        }();
+        if (rc != 0)
+        {
+            return DatabaseResult(DatabaseError(DatabaseErrorCode.ConnectionFailed, lmdbStr(rc)));
+        }
+
+        return NoDatabaseError;
+    }
+
+    /**
+     * Close
+     *
+     */
+    override void close() @safe @nogc nothrow
+    {
+        if (env is null)
+        {
+            return;
+        }
+
+        () @trusted { mdb_env_close(env); }();
+    }
+
+    /**
+     * Construct a new LMDB specific RO transaction
+     */
+    override Transaction readOnlyTransaction() @safe
+    {
+        return null;
+    }
+
+    /**
+     * Construct a new LMDB specific RW transaction
+     */
+    override Transaction readWriteTransaction() @safe
+    {
+        return null;
+    }
+
+private:
+
+    /* MDB environment */
+    MDB_env* env;
+}
--- source/moss/db/keyvalue/driver/lmdb/package.d
+++ source/moss/db/keyvalue/driver/lmdb/package.d
@@ -7,7 +7,7 @@
 /**
  * moss.db.keyvalue.driver.lmdb
  *
- * Key Value DB implemented using LMDB
+ * KModule level imports
  *
  * Authors: Copyright © 2020-2022 Serpent OS Developers
  * License: Zlib
@@ -15,103 +15,13 @@
 
 module moss.db.keyvalue.driver.lmdb;
 
-public import moss.db.keyvalue.driver;
-import moss.db.keyvalue.errors;
-import moss.db.keyvalue.interfaces;
+public import moss.db.keyvalue.driver.lmdb.driver;
+public import moss.db.keyvalue.driver.lmdb.transaction;
 
-import std.conv : octal;
-import std.string : toStringz;
-
-import lmdb;
-
-static private string lmdbStr(int rcCode) @trusted nothrow @nogc
+static package string lmdbStr(int rcCode) @trusted nothrow @nogc
 {
     import std.string : fromStringz;
+    import lmdb : mdb_strerror;
 
     return cast(string) mdb_strerror(rcCode).fromStringz;
-}
-
-/**
- * Implementation using LMDB
- */
-public final class LMDBDriver : Driver
-{
-    /**
-     * Open LMDB environment + open from a given path
-     *
-     * Params:
-     *      uri = Resource locator string
-     *      flags = Flags that affect opening
-     * Returns: A nullable error
-     */
-    override DatabaseResult connect(const(string) uri, DatabaseFlags flags) @safe nothrow
-    {
-        /* no env dir */
-        int cFlags = MDB_NOSUBDIR;
-
-        /* Read-only? */
-        if ((flags & DatabaseFlags.ReadOnly) == DatabaseFlags.ReadOnly)
-        {
-            cFlags |= MDB_RDONLY;
-        }
-
-        /* no sync ? */
-        if ((flags & DatabaseFlags.DisableSync) == DatabaseFlags.DisableSync)
-        {
-            cFlags |= MDB_NOSYNC;
-        }
-
-        /* Create environment first */
-        int rc = () @trusted { return mdb_env_create(&env); }();
-        if (rc != 0)
-        {
-            return DatabaseResult(DatabaseError(DatabaseErrorCode.ConnectionFailed, lmdbStr(rc)));
-        }
-
-        /* Open 0600 connection to the DB */
-        rc = () @trusted {
-            return mdb_env_open(env, uri.toStringz, cFlags, octal!600);
-        }();
-        if (rc != 0)
-        {
-            return DatabaseResult(DatabaseError(DatabaseErrorCode.ConnectionFailed, lmdbStr(rc)));
-        }
-
-        return NoDatabaseError;
-    }
-
-    /**
-     * Close
-     *
-     */
-    override void close() @safe @nogc nothrow
-    {
-        if (env is null)
-        {
-            return;
-        }
-
-        () @trusted { mdb_env_close(env); }();
-    }
-
-    /**
-     * Construct a new LMDB specific RO transaction
-     */
-    override Transaction readOnlyTransaction() @safe
-    {
-        return null;
-    }
-
-    /**
-     * Construct a new LMDB specific RW transaction
-     */
-    override Transaction readWriteTransaction() @safe
-    {
-        return null;
-    }
-
-private:
-
-    /* MDB environment */
-    MDB_env* env;
 }
