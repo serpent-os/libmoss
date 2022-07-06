226bbd2c457f4c3472178f678efd950dba7c43af
Ikey Doherty <ikey@serpentos.com>
2022-07-06 20:23:30 +0100

    lmdb: Placeholder transaction
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/db/keyvalue/driver/lmdb/transaction.d
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.db.keyvalue.driver.lmdb
+ *
+ * LMDB Transactions
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.db.keyvalue.driver.lmdb.transaction;
+
+public import moss.db.keyvalue.driver;
+import moss.db.keyvalue.errors;
+import moss.db.keyvalue.interfaces;
+
+import lmdb;
+
+public class LMDBTransaction /*: Transaction */
+{
+
+}
