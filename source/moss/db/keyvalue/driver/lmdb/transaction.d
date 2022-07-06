0f6536ba7d595e1c0a625b088177fa68a11ff5b3
Ikey Doherty <ikey@serpentos.com>
2022-07-06 20:32:21 +0100

    lmdb/transaction: Noop all the API
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/db/keyvalue/driver/lmdb/transaction.d
+++ source/moss/db/keyvalue/driver/lmdb/transaction.d
@@ -18,10 +18,56 @@ module moss.db.keyvalue.driver.lmdb.transaction;
 public import moss.db.keyvalue.driver;
 import moss.db.keyvalue.errors;
 import moss.db.keyvalue.interfaces;
+import moss.db.keyvalue.driver.lmdb.driver : LMDBDriver;
 
 import lmdb;
 
-public class LMDBTransaction /*: Transaction */
+/**
+ * LMDB Transaction implementation.
+ */
+package class LMDBTransaction : Transaction
 {
+    @disable this();
 
+    /**
+     * Construct a driver with the given parent
+     */
+    this(LMDBDriver parentDriver) @safe @nogc nothrow
+    {
+        this.parentDriver = parentDriver;
+    }
+
+public:
+
+    override pure Bucket bucket(in string name) const return @safe
+    {
+        return Bucket.init;
+    }
+
+    override void set(in Bucket bucket, in ImmutableDatum key, in ImmutableDatum value) return @safe
+    {
+    }
+
+    override void remove(in Bucket bucket, in ImmutableDatum key) return @safe
+    {
+
+    }
+
+    override BucketIterator iterator(in Bucket bucket) return @safe
+    {
+        return null;
+    }
+
+    override void removeBucket(in Bucket bucket) return @safe
+    {
+    }
+
+    override ImmutableDatum get(in Bucket bucket, in ImmutableDatum key) const return @safe
+    {
+        return null;
+    }
+
+private:
+
+    LMDBDriver parentDriver;
 }
