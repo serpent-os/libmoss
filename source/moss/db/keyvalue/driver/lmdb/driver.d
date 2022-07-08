7ddfcc7bd8c4912edf3db367a53b00a2af68a9b4
Ikey Doherty <ikey@serpentos.com>
2022-07-08 10:45:13 +0100

    lmdb/driver: cap to 2 dbs
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/db/keyvalue/driver/lmdb/driver.d
+++ source/moss/db/keyvalue/driver/lmdb/driver.d
@@ -95,6 +95,13 @@ public final class LMDBDriver : Driver
             return DatabaseResult(DatabaseError(DatabaseErrorCode.ConnectionFailed, lmdbStr(rc)));
         }
 
+        /* We need 2 DBs - main and meta */
+        rc = () @trusted { return mdb_env_set_maxdbs(env, 2); }();
+        if (rc != 0)
+        {
+            return DatabaseResult(DatabaseError(DatabaseErrorCode.InternalDriver, lmdbStr(rc)));
+        }
+
         /* Open 0600 connection to the DB */
         rc = () @trusted {
             return mdb_env_open(env, uri.toStringz, cFlags, octal!644);
