97a43e7fa8eda42414f4644d5c026781d5bf6baa
Ikey Doherty <ikey@serpentos.com>
2023-03-03 22:57:00 +0000

    yml_helper: General cleanup for build logs
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/format/source/yml_helper.d
+++ source/moss/format/source/yml_helper.d
@@ -35,34 +35,18 @@ void setValue(T)(ref Node node, ref T value, YamlSchema schema)
     static if (is(T == int64_t))
     {
         value = node.as!int64_t;
-        debug
-        {
-            //trace("    '- <int64_t>");
-        }
     }
     else static if (is(T == uint64_t))
     {
         value = node.as!uint64_t;
-        debug
-        {
-            //trace("    '- <uint64_t>");
-        }
     }
     else static if (is(T == bool))
     {
         value = node.as!bool;
-        debug
-        {
-            //trace("    '- <bool>");
-        }
     }
     else
     {
         value = node.as!string;
-        debug
-        {
-            //trace(format!"    '- <string> (= '%s')"(value));
-        }
         if (schema.acceptableValues.length < 1)
         {
             return;
@@ -88,24 +72,14 @@ void setValueArray(T)(ref Node node, ref T value)
         static if (is(T == string) || is(T == string[]))
         {
     case NodeID.scalar:
+            /* Scalar value */
             value ~= node.as!string;
-            debug
-            {
-                //trace("    '- <string>");
-            }
             break;
     case NodeID.sequence:
-            debug
-            {
-                //trace("    '- sequence of <string> scalars:");
-            }
+            /* Sequence */
             foreach (ref Node v; node)
             {
                 value ~= v.as!string;
-                debug
-                {
-                    //trace(format!"     '- '%s'"(v.as!string));
-                }
             }
             break;
         }
@@ -113,28 +87,15 @@ void setValueArray(T)(ref Node node, ref T value)
         {
     case NodeID.scalar:
             value ~= node.as!(typeof(value[0]));
-            debug
-            {
-                //trace(format!"    '- <%s>"(node.as!(typeof(value[0]))));
-            }
             break;
     case NodeID.sequence:
-            debug
-            {
-                //trace("    '- sequence of scalars:");
-            }
             foreach (ref Node v; node)
             {
                 value ~= v.as!(typeof(value[0]));
-                debug
-                {
-                    //trace(format!"     '- '%s' as <%s>"(v, typeof(value[0])));
-                }
             }
             break;
         }
     default:
-        //trace(format!"    '- node.nodeID %s not parsed?"(node.nodeID));
         break;
     }
 }
@@ -152,11 +113,6 @@ void parseSection(T)(ref Node node, ref T section) @system
     /* Walk the members in the type T section struct -- the order is undefined */
     static foreach (member; __traits(allMembers, T))
     {
-        /*
-         * The extra set of brackets ensures that each compile time loop
-         * gets a clean scope with no duplicate declarations
-         * (otherwise the compiler complains vociferously)
-         */
         {
             mixin("import " ~ moduleName!T ~ ";");
 
@@ -164,12 +120,6 @@ void parseSection(T)(ref Node node, ref T section) @system
             mixin("enum hasYamlSchema = hasUDA!(" ~ T.stringof ~ "." ~ member ~ ", YamlSchema);");
             static if (hasYamlSchema)
             {
-                debug
-                {
-                    /* Add a compile-time message each time a field with a YamlSchema is found */
-                    pragma(msg, ">>> YamlSchema found for: ", T, ".", member);
-                }
-
                 mixin("enum udaID = getUDAs!(" ~ T.stringof ~ "." ~ member ~ ", YamlSchema);");
                 static if (udaID.length == 1)
                 {
@@ -184,33 +134,19 @@ void parseSection(T)(ref Node node, ref T section) @system
                         enforce(node.containsKey(yamlName), "Missing mandatory key: " ~ yamlName);
                     }
 
+                    /* Single value */
                     static if (type == YamlType.Single)
                     {
                         if (node.containsKey(yamlName))
                         {
-                            debug
-                            {
-                                //trace(format!"  '- Parsing YamlSchema for member: %s"(member));
-                            }
-                            debug
-                            {
-                                //trace(format!"   '- Parsing YAML key '%s':"(yamlName));
-                            }
                             mixin("setValue(node[yamlName], section." ~ member ~ ", udaID);");
                         }
                     }
-                    else static if (type == YamlType.Array)
+                    /* Array */
+                else static if (type == YamlType.Array)
                     {
                         if (node.containsKey(yamlName))
                         {
-                            debug
-                            {
-                                //trace(format!"  '- Parsing YamlSchema for member: %s"(member));
-                            }
-                            debug
-                            {
-                                //trace(format!"   '- Parsing YAML key '%s':"(yamlName));
-                            }
                             mixin("setValueArray(node[yamlName], section." ~ member ~ ");");
                         }
                     }
